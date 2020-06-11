ef2ac3fbe2fee3a37ad09fb893787192b507cdd0
Tinus Strydom <tinuss@loans-inc.co.za>
2020-06-11 12:36:13 +0200

    added exact path to route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,11 +6,11 @@ import './App.css';
 import Aside from '../../components/Aside';
 import Nav from '../../components/Nav';
 import Content from '../../components/Content';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Redirect} from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router exact path="/">
         <Aside/>
         <Nav/>
         <Content/>
